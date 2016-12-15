e541e591ad55fbbe7c1f76dedf993c21534ab0c4
Phil Murray <philmurray2.0@gmail.com>
2016-12-15 10:56:46 +0000

    set up script to flash arduino with latest on startup

---
+++ arduino/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/Lampi/arduino
+
+md5_file="md5hash"
+hex_file="src/src.ino.with_bootloader.hex"
+conf_file="avrdude.conf"
+
+old_md5=$(cat "$md5_file")
+new_md5=$(md5sum "$hex_file")
+if [ "$old_md5" != "$new_md5" ];
+then
+	echo "updated arduino file found.  Updating"
+	sudo avrdude -p atmega328p -C "$conf_file" -c pi_1 -U flash:w:$hex_file:i && echo "$new_md5" > "$md5_file"
+else
+	echo "arduino file has not changed.  Skipping"
+fi
+
+cd -
--- startup.sh
+++ startup.sh
@@ -3,3 +3,4 @@
 cd /home/pi/Lampi
 git pull --rebase
 pip3 install -r requirements.txt
+./arduino/install.sh
