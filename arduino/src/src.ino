cdd15ac0b4fce1f91e65285a4226edce4ad00599
Phil Murray <philmurray2.0@gmail.com>
2016-12-15 23:23:57 -0500

    testing out serial with neopixel

--- arduino/src/src.ino
+++ arduino/src/src.ino
@@ -1,13 +1,41 @@
-byte number = 0;
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define NEOPIXEL_PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, NEOPIXEL_PIN, NEO_RGBW + NEO_KHZ800);
+char serial_char = 0;
 
 void setup(){
-Serial.begin(9600);
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  
+  strip.begin();
+  strip.show();
 }
 
 void loop(){
-if (Serial.available()) {
-number = Serial.read();
-Serial.print("character recieved: ");
-Serial.println(number, DEC);
+  if (Serial.available()) {
+    serial_char = Serial.read();
+    if (serial_char == 'w')
+    {
+      all_white(255);
+    }
+    else if (serial_char == 'd')
+    {
+      all_white(100);
+    }
+  }
+  
 }
+
+void all_white(uint8_t b) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0,0,0,b));
+    strip.show();
+  }
 }
+
+
